cb111fd419c0adb989cbe6d02b8b699ab1147627
KaberaSamuel <kaberanshutis@gmail.com>
2025-12-18 01:14:33 +0200

    moved utility methods to Helper.java

---
+++ algorithms-basics/Helper.java
@@ -0,0 +1,61 @@
+public class Helper {
+
+    public static int smallest(int[] array) {
+        // initialize smallest with the first number
+        int smallestSofar = array[0];
+
+        for (int number : array) {
+            if (number < smallestSofar) {
+                smallestSofar = number;
+            }
+        }
+
+        return smallestSofar;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int smallestItemIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int currentNumber = array[i];
+            int smallestNumberSofar = array[smallestItemIndex];
+
+            if (currentNumber < smallestNumberSofar) {
+                smallestItemIndex = i;
+            }
+        }
+
+        return smallestItemIndex;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        // start with the last item
+        int smallestItemIndex = array.length - 1;
+
+        for (int i = 0; i < array.length; i++) {
+            // skip items that are below the range
+            if (i < startIndex) {
+                continue;
+            }
+
+            int currentNumber = array[i];
+            int smallestNumberSofar = array[smallestItemIndex];
+
+            if (currentNumber < smallestNumberSofar) {
+                smallestItemIndex = i;
+            }
+        }
+
+        return smallestItemIndex;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int item1 = array[index1];
+        int item2 = array[index2];
+
+        // swap them
+        array[index1] = item2;
+        array[index2] = item1;
+    }
+
+}
--- algorithms-basics/Program.java
+++ algorithms-basics/Program.java
@@ -1,57 +1,16 @@
+import java.util.Arrays;
+
 public class Program {
     public static void main(String[] args) {
-        int[] numbers = { -1, 6, 9, 8, 12 };
-        System.out.println(indexOfSmallestFrom(numbers, 0));
-        System.out.println(indexOfSmallestFrom(numbers, 1));
-        System.out.println(indexOfSmallestFrom(numbers, 2));
+        int[] numbers = { 3, 2, 5, 4, 8 };
+
+        System.out.println(Arrays.toString(numbers));
+
+        Helper.swap(numbers, 1, 0);
+        System.out.println(Arrays.toString(numbers));
+
+        Helper.swap(numbers, 0, 3);
+        System.out.println(Arrays.toString(numbers));
     }
 
-    public static int smallest(int[] array) {
-        // initialize smallest with the first number
-        int smallestSofar = array[0];
-
-        for (int number : array) {
-            if (number < smallestSofar) {
-                smallestSofar = number;
-            }
-        }
-
-        return smallestSofar;
-    }
-
-    public static int indexOfSmallest(int[] array) {
-        int smallestItemIndex = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            int currentNumber = array[i];
-            int smallestNumberSofar = array[smallestItemIndex];
-
-            if (currentNumber < smallestNumberSofar) {
-                smallestItemIndex = i;
-            }
-        }
-
-        return smallestItemIndex;
-    }
-
-    public static int indexOfSmallestFrom(int[] array, int startIndex) {
-        // start with the last item
-        int smallestItemIndex = array.length - 1;
-
-        for (int i = 0; i < array.length; i++) {
-            // skip items that are below the range
-            if (i < startIndex) {
-                continue;
-            }
-
-            int currentNumber = array[i];
-            int smallestNumberSofar = array[smallestItemIndex];
-
-            if (currentNumber < smallestNumberSofar) {
-                smallestItemIndex = i;
-            }
-        }
-
-        return smallestItemIndex;
-    }
 }
